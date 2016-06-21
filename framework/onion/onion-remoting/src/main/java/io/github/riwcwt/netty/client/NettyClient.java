467a82cb92b9e1906181e83a0983db2cb57347af
zhu jun <riwcwt@gmail.com>
2016-06-21 22:17:04 +0800

    netty

--- framework/onion/onion-remoting/src/main/java/io/github/riwcwt/netty/client/NettyClient.java
+++ framework/onion/onion-remoting/src/main/java/io/github/riwcwt/netty/client/NettyClient.java
@@ -1,10 +1,13 @@
 package io.github.riwcwt.netty.client;
 
+import java.net.InetSocketAddress;
+
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class NettyClient implements ApplicationContextAware {
+public class NettyClient implements ApplicationContextAware, InitializingBean {
 
 	private ApplicationContext context = null;
 
@@ -13,4 +16,12 @@ public class NettyClient implements ApplicationContextAware {
 		this.context = applicationContext;
 	}
 
+	public void connect(InetSocketAddress socketAddress) {
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 }
