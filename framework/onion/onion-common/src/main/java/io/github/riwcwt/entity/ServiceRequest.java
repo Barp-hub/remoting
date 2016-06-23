81fff4bbe294a5faa231bec038279c2113e4fd80
zhu jun <riwcwt@gmail.com>
2016-06-23 22:51:22 +0800

    codec

---
+++ framework/onion/onion-common/src/main/java/io/github/riwcwt/entity/ServiceRequest.java
@@ -0,0 +1,59 @@
+package io.github.riwcwt.entity;
+
+public class ServiceRequest {
+	private String requestId;
+	private String service;
+	private String version;
+	private String method;
+	private Class<?>[] parameterTypes;
+	private Object[] parameters;
+
+	public String getRequestId() {
+		return requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+
+	public Class<?>[] getParameterTypes() {
+		return parameterTypes;
+	}
+
+	public void setParameterTypes(Class<?>[] parameterTypes) {
+		this.parameterTypes = parameterTypes;
+	}
+
+	public Object[] getParameters() {
+		return parameters;
+	}
+
+	public void setParameters(Object[] parameters) {
+		this.parameters = parameters;
+	}
+
+}
