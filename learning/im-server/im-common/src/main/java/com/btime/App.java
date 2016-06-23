3fae7be4cbb2cb706517ee3a60abff7165511260
zhu jun <riwcwt@gmail.com>
2016-06-23 18:49:56 +0800

    mqtt

---
+++ learning/im-server/im-common/src/main/java/com/btime/App.java
@@ -0,0 +1,13 @@
+package com.btime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
