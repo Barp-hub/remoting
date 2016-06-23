25aeddd8381c4e4b8dd4a51c82261c1f12aad85b
zhu jun <riwcwt@gmail.com>
2016-06-23 17:31:56 +0800

    entity

---
+++ framework/onion/onion-common/src/main/java/io/github/riwcwt/entity/Request.java
@@ -0,0 +1,14 @@
+package io.github.riwcwt.entity;
+
+public class Request {
+	private Integer type;
+
+	public Integer getType() {
+		return type;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+}
