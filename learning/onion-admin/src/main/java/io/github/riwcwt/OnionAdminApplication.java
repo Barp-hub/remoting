e31e16a8d69c8db1c7a180c4e99b0714d6df7317
zhu jun <riwcwt@gmail.com>
2016-07-03 22:55:29 +0800

    server

---
+++ learning/onion-admin/src/main/java/io/github/riwcwt/OnionAdminApplication.java
@@ -0,0 +1,12 @@
+package io.github.riwcwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnionAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnionAdminApplication.class, args);
+	}
+}
