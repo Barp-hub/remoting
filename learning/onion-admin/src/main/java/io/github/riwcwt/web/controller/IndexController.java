2e909243baa10804d7acf68aae8d9f4e2d419818
zhu jun <riwcwt@gmail.com>
2016-07-04 09:57:51 +0800

    remoting

--- learning/onion-admin/src/main/java/io/github/riwcwt/web/controller/IndexController.java
+++ learning/onion-admin/src/main/java/io/github/riwcwt/web/controller/IndexController.java
@@ -1,8 +1,17 @@
 package io.github.riwcwt.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("user", "freemarker");
+		return "index";
+	}
 
 }
---
+++ learning/onion-admin/src/main/resources/templates/index.ftl
@@ -0,0 +1,8 @@
+<html>
+<head>
+  <title>Welcome!</title>
+</head>
+<body>
+  <h3>Welcome ${user}!</h3>
+</body>
+</html>
