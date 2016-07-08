a241ffb9aaf14925f7e891c2142fb846cbc06aba
zhu jun <riwcwt@gmail.com>
2016-07-08 13:07:32 +0800

    remoting

--- framework/onion/onion/src/main/java/io/github/riwcwt/config/OnionConfig.java
+++ framework/onion/onion/src/main/java/io/github/riwcwt/config/OnionConfig.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import io.github.riwcwt.netty.config.NettyConfig;
+import io.github.riwcwt.proxy.config.ProxyConfig;
 
 @Configuration
-@Import(value = { NettyConfig.class })
+@Import(value = { NettyConfig.class, ProxyConfig.class })
 public class OnionConfig {
 
 }
