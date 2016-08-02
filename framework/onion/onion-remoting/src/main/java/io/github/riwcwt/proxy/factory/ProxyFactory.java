10cd3dd84d63a152211a7c4ad65f67c6f05d3a0f
zhu jun <riwcwt@gmail.com>
2016-08-02 13:11:05 +0800

    remoting

--- framework/onion/onion-remoting/src/main/java/io/github/riwcwt/proxy/factory/ProxyFactory.java
+++ framework/onion/onion-remoting/src/main/java/io/github/riwcwt/proxy/factory/ProxyFactory.java
@@ -2,22 +2,37 @@ package io.github.riwcwt.proxy.factory;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.net.InetSocketAddress;
+import java.util.Calendar;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import io.github.riwcwt.constant.MessageType;
+import io.github.riwcwt.entity.Request;
 import io.github.riwcwt.entity.ServiceRequest;
+import io.github.riwcwt.netty.client.NettyClient;
 
 @Component
 public class ProxyFactory implements InvocationHandler {
 
+	@Autowired
+	private NettyClient client = null;
+
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		ServiceRequest request = new ServiceRequest();
-		request.setService(method.getClass().getCanonicalName());
-		request.setMethod(method.getName());
-		request.setParameterTypes(method.getParameterTypes());
-		request.setParameters(args);
+		ServiceRequest serviceRequest = new ServiceRequest();
+		serviceRequest.setService(method.getClass().getCanonicalName());
+		serviceRequest.setMethod(method.getName());
+		serviceRequest.setParameterTypes(method.getParameterTypes());
+		serviceRequest.setParameters(args);
 
+		Request request = new Request();
+		request.setData(serviceRequest);
+		request.setRequestId(String.valueOf(Calendar.getInstance().getTimeInMillis()));
+		request.setType(MessageType.NORMAL_REQUEST);
+
+		this.client.send(new InetSocketAddress("localhost", 8888), request);
 		return "proxy";
 	}
 
