f45bd4c35b37926ae865c9614c568af5a32ed560
zhu jun <riwcwt@gmail.com>
2016-11-17 09:56:09 +0800

    初始化

--- learning/hello-spi/src/main/java/io/github/riwcwt/App.java
+++ learning/hello-spi/src/main/java/io/github/riwcwt/App.java
@@ -4,10 +4,8 @@ package io.github.riwcwt;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
