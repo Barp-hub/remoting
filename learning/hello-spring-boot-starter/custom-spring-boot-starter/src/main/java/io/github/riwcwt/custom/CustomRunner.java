727b2a4abd8e719d899eec0d5d01123fe7f7795b
zhu jun <riwcwt@gmail.com>
2016-12-14 09:47:48 +0800

    初始化

--- learning/hello-spring-boot-starter/custom-spring-boot-starter/src/main/java/io/github/riwcwt/custom/CustomRunner.java
+++ learning/hello-spring-boot-starter/custom-spring-boot-starter/src/main/java/io/github/riwcwt/custom/CustomRunner.java
@@ -1,16 +1,20 @@
 package io.github.riwcwt.custom;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.support.AbstractApplicationContext;
 
 public class CustomRunner implements CommandLineRunner {
 
+	private static final Logger logger = LoggerFactory.getLogger(CustomRunner.class);
+
 	@Autowired
 	private AbstractApplicationContext applicationContext;
 
 	public void run(String... args) throws Exception {
-
+		logger.info(applicationContext.getApplicationName());
 	}
 
 }
