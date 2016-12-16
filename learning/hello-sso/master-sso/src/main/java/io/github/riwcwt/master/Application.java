e97ab1bd4158102b8eae5c9e6c08044c616946ac
zhu jun <riwcwt@gmail.com>
2016-12-16 14:14:01 +0800

    初始化

--- learning/hello-sso/master-sso/src/main/java/io/github/riwcwt/master/Application.java
+++ learning/hello-sso/master-sso/src/main/java/io/github/riwcwt/master/Application.java
@@ -1,4 +1,4 @@
-package io.github.riwcwt;
+package io.github.riwcwt.master;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- learning/hello-sso/master-sso/src/main/java/io/github/riwcwt/master/controller/IndexController.java
+++ learning/hello-sso/master-sso/src/main/java/io/github/riwcwt/master/controller/IndexController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index() {
-		return "index";
-	}
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
 
 }
