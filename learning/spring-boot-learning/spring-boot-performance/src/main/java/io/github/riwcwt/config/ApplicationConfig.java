bb91ad8032661a98958619b42cafeae9cbeef45e
zhu jun <riwcwt@gmail.com>
2016-12-30 17:07:56 +0800

    初始化

--- learning/spring-boot-learning/spring-boot-performance/src/main/java/io/github/riwcwt/config/ApplicationConfig.java
+++ learning/spring-boot-learning/spring-boot-performance/src/main/java/io/github/riwcwt/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package io.github.riwcwt.config;
 
+import org.apache.coyote.ProtocolHandler;
 import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
@@ -18,12 +19,15 @@ public class ApplicationConfig {
             if (container instanceof TomcatEmbeddedServletContainerFactory) {
                 TomcatEmbeddedServletContainerFactory tomcat = TomcatEmbeddedServletContainerFactory.class.cast(container);
                 tomcat.addConnectorCustomizers(connector -> {
-                    Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
-                    //设置最大连接数
-                    protocol.setMaxConnections(4096);
-                    //设置最大线程数
-                    protocol.setMaxThreads(1000);
-                    protocol.setConnectionTimeout(30000);
+                    ProtocolHandler handler = connector.getProtocolHandler();
+                    if (handler instanceof Http11NioProtocol) {
+                        Http11NioProtocol protocol = Http11NioProtocol.class.cast(handler);
+                        //设置最大连接数
+                        protocol.setMaxConnections(10000);
+                        //设置最大线程数
+                        protocol.setMaxThreads(1000);
+                        protocol.setConnectionTimeout(30000);
+                    }
                 });
             }
         };
