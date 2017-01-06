c0500615d962fff4d60ce49433f4e876e615a43f
zhu jun <riwcwt@gmail.com>
2017-01-06 16:51:21 +0800

    初始化

--- learning/etcd-learning/hello-etcd/src/test/java/com/coreos/jetcd/EtcdAuthClientTest.java
+++ learning/etcd-learning/hello-etcd/src/test/java/com/coreos/jetcd/EtcdAuthClientTest.java
@@ -1,41 +1,39 @@
 package com.coreos.jetcd;
 
-import java.nio.charset.Charset;
-import java.util.concurrent.ExecutionException;
-
-import org.junit.Test;
-import org.testng.annotations.BeforeTest;
-import org.testng.asserts.Assertion;
-
 import com.coreos.jetcd.api.AuthRoleGetResponse;
 import com.coreos.jetcd.api.Permission;
 import com.coreos.jetcd.api.RangeResponse;
 import com.coreos.jetcd.exception.AuthFailedException;
 import com.coreos.jetcd.exception.ConnectException;
 import com.google.protobuf.ByteString;
-
 import io.grpc.StatusRuntimeException;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import java.nio.charset.Charset;
+import java.util.concurrent.ExecutionException;
 
 /**
  * test etcd auth
  */
 public class EtcdAuthClientTest {
 
-    private EtcdAuth   authClient;
-    private EtcdKV     kvClient;
+    private EtcdAuth authClient;
+    private EtcdKV kvClient;
 
-    private ByteString roleName      = ByteString.copyFromUtf8("root");
+    private ByteString roleName = ByteString.copyFromUtf8("root");
 
     private ByteString keyRangeBegin = ByteString.copyFromUtf8("foo");
-    private ByteString keyRangeEnd   = ByteString.copyFromUtf8("zoo");
+    private ByteString keyRangeEnd = ByteString.copyFromUtf8("zoo");
 
-    private ByteString testKey       = ByteString.copyFromUtf8("foo1");
-    private ByteString testName      = ByteString.copyFromUtf8("bar");
+    private ByteString testKey = ByteString.copyFromUtf8("foo1");
+    private ByteString testName = ByteString.copyFromUtf8("bar");
 
-    private ByteString userName      = ByteString.copyFrom("root", Charset.defaultCharset());
-    private ByteString password      = ByteString.copyFrom("123", Charset.defaultCharset());
+    private ByteString userName = ByteString.copyFrom("root", Charset.defaultCharset());
+    private ByteString password = ByteString.copyFrom("123", Charset.defaultCharset());
 
-    private Assertion  test;
+    private Assertion test;
 
     private EtcdClient etcdClient;
     private EtcdClient authEtcdClient;
@@ -54,7 +52,7 @@ public class EtcdAuthClientTest {
     /**
      * create role with un-auth etcd client
      */
-    @Test(groups = "role")
+    //@Test(groups = "role")
     public void testRoleAdd() throws ExecutionException, InterruptedException {
         this.authClient.roleAdd(roleName).get();
     }
@@ -62,7 +60,7 @@ public class EtcdAuthClientTest {
     /**
      * grant permission to role
      */
-    @Test(dependsOnMethods = "testRoleAdd", groups = "role")
+    //@Test(dependsOnMethods = "testRoleAdd", groups = "role")
     public void testRoleGrantPermission() throws ExecutionException, InterruptedException {
         this.authClient.roleGrantPermission(roleName, keyRangeBegin, keyRangeEnd, Permission.Type.READWRITE).get();
     }
@@ -70,7 +68,8 @@ public class EtcdAuthClientTest {
     /**
      * add user with password and username
      */
-    @Test(groups = "user")
+    //@Test(groups = "user")
+    @Test
     public void testUserAdd() throws ExecutionException, InterruptedException {
         this.authClient.userAdd(userName, password).get();
     }
@@ -78,7 +77,7 @@ public class EtcdAuthClientTest {
     /**
      * grant user role
      */
-    @Test(dependsOnMethods = { "testUserAdd", "testRoleGrantPermission" }, groups = "user")
+    //@Test(dependsOnMethods = { "testUserAdd", "testRoleGrantPermission" }, groups = "user")
     public void testUserGrantRole() throws ExecutionException, InterruptedException {
         this.authClient.userGrantRole(userName, roleName).get();
     }
@@ -86,7 +85,7 @@ public class EtcdAuthClientTest {
     /**
      * enable etcd auth
      */
-    @Test(dependsOnGroups = "user", groups = "authEnable")
+    //@Test(dependsOnGroups = "user", groups = "authEnable")
     public void testEnableAuth() throws ExecutionException, InterruptedException {
         this.authClient.authEnable().get();
     }
@@ -94,7 +93,7 @@ public class EtcdAuthClientTest {
     /**
      * auth client with password and user name
      */
-    @Test(dependsOnMethods = "testEnableAuth", groups = "authEnable")
+    //@Test(dependsOnMethods = "testEnableAuth", groups = "authEnable")
     public void setupAuthClient() throws AuthFailedException, ConnectException {
         this.authEtcdClient = EtcdClientBuilder.newBuilder().endpoints("localhost:2379").setName(userName).setPassword(password).build();
 
@@ -103,7 +102,7 @@ public class EtcdAuthClientTest {
     /**
      * put and range with auth client
      */
-    @Test(groups = "testAuth", dependsOnGroups = "authEnable")
+    //@Test(groups = "testAuth", dependsOnGroups = "authEnable")
     public void testKVWithAuth() throws ExecutionException, InterruptedException {
         Throwable err = null;
         try {
@@ -119,7 +118,7 @@ public class EtcdAuthClientTest {
     /**
      * put and range with auth client
      */
-    @Test(groups = "testAuth", dependsOnGroups = "authEnable")
+    //@Test(groups = "testAuth", dependsOnGroups = "authEnable")
     public void testKVWithoutAuth() throws InterruptedException {
         Throwable err = null;
         try {
@@ -134,7 +133,7 @@ public class EtcdAuthClientTest {
     /**
      * get auth's permission
      */
-    @Test(groups = "testAuth", dependsOnGroups = "authEnable")
+    //@Test(groups = "testAuth", dependsOnGroups = "authEnable")
     public void testRoleGet() throws ExecutionException, InterruptedException {
         AuthRoleGetResponse roleGetResponse = this.authEtcdClient.getAuthClient().roleGet(roleName).get();
         this.test.assertTrue(roleGetResponse.getPermCount() != 0);
@@ -143,7 +142,7 @@ public class EtcdAuthClientTest {
     /**
      * disable etcd auth
      */
-    @Test(dependsOnGroups = "testAuth", groups = "disableAuth")
+    //@Test(dependsOnGroups = "testAuth", groups = "disableAuth")
     public void testDisableAuth() {
         Throwable err = null;
         try {
@@ -157,7 +156,7 @@ public class EtcdAuthClientTest {
     /**
      * delete user
      */
-    @Test(dependsOnGroups = "disableAuth", groups = "clearEnv")
+    //@Test(dependsOnGroups = "disableAuth", groups = "clearEnv")
     public void delUser() {
         Throwable err = null;
         try {
@@ -171,7 +170,7 @@ public class EtcdAuthClientTest {
     /**
      * delete role
      */
-    @Test(dependsOnGroups = "disableAuth", groups = "clearEnv")
+    //@Test(dependsOnGroups = "disableAuth", groups = "clearEnv")
     public void delRole() {
         Throwable err = null;
         try {
--- learning/etcd-learning/hello-etcd/src/test/java/io/github/riwcwt/ApplicationTests.java
+++ learning/etcd-learning/hello-etcd/src/test/java/io/github/riwcwt/ApplicationTests.java
@@ -1,16 +1,58 @@
 package io.github.riwcwt;
 
+import com.coreos.jetcd.api.KVGrpc;
+import com.coreos.jetcd.api.PutRequest;
+import com.coreos.jetcd.api.PutResponse;
+import com.coreos.jetcd.api.WatchCreateRequest;
+import com.coreos.jetcd.api.WatchRequest;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.protobuf.ByteString;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.nio.charset.Charset;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 public class ApplicationTests {
 
-	@Test
-	public void etcd() {
-	}
+    private ManagedChannel channel = null;
+
+    @Before
+    public void setup() {
+        //ManagedChannel channel = NettyChannelBuilder.forTarget("server://localhost:11111")
+        //        .nameResolverFactory(new ServerNameResolverProvider())
+        //        .loadBalancerFactory(RoundRobinLoadBalancerFactory.getInstance()).usePlaintext(true).build();
+
+        channel = ManagedChannelBuilder.forAddress("localhost", 2379).usePlaintext(true).build();
+    }
+
+    @Test
+    public void getKV() throws ExecutionException, InterruptedException {
+
+
+        KVGrpc.KVFutureStub kvGrpc = KVGrpc.newFutureStub(channel);
+
+        PutRequest request = PutRequest.newBuilder().setKey(ByteString.copyFrom("hello", Charset.forName("UTF-8")))
+                .setValue(ByteString.copyFrom("world", Charset.forName("UTF-8"))).setLease(0).setPrevKv(false).build();
+
+        ListenableFuture<PutResponse> response = kvGrpc.put(request);
+
+        System.out.println(response.get().toString());
+
+    }
+
+    @Test
+    public void watch() {
+        WatchRequest.newBuilder().setCreateRequest(WatchCreateRequest.newBuilder().setKey(ByteString.copyFrom("hello", Charset.forName("UTF-8")))).build();
+    }
+
+    @After
+    public void destroy() throws InterruptedException {
+        channel.shutdown().awaitTermination(10, TimeUnit.SECONDS);
+    }
 
 }
