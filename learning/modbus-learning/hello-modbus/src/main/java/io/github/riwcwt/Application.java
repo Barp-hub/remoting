2625fc1042705838da5f7dee694dac35d0be5f9d
zhu jun <riwcwt@gmail.com>
2017-01-24 22:10:54 +0800

    modbus

--- learning/modbus-learning/hello-modbus/src/main/java/io/github/riwcwt/Application.java
+++ learning/modbus-learning/hello-modbus/src/main/java/io/github/riwcwt/Application.java
@@ -6,6 +6,9 @@ import java.util.concurrent.ExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.netty.util.internal.logging.InternalLoggerFactory;
+import io.netty.util.internal.logging.Log4JLoggerFactory;
+
 /**
  * Hello world!
  *
@@ -14,6 +17,9 @@ public class Application {
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+
+		InternalLoggerFactory.setDefaultFactory(new Log4JLoggerFactory());
+
 		Slave slave = new Slave();
 		slave.start();
 
--- learning/modbus-learning/hello-modbus/src/main/java/io/github/riwcwt/Slave.java
+++ learning/modbus-learning/hello-modbus/src/main/java/io/github/riwcwt/Slave.java
@@ -2,8 +2,24 @@ package io.github.riwcwt;
 
 import java.util.concurrent.ExecutionException;
 
+import com.digitalpetri.modbus.requests.MaskWriteRegisterRequest;
+import com.digitalpetri.modbus.requests.ReadCoilsRequest;
+import com.digitalpetri.modbus.requests.ReadDiscreteInputsRequest;
 import com.digitalpetri.modbus.requests.ReadHoldingRegistersRequest;
+import com.digitalpetri.modbus.requests.ReadInputRegistersRequest;
+import com.digitalpetri.modbus.requests.WriteMultipleCoilsRequest;
+import com.digitalpetri.modbus.requests.WriteMultipleRegistersRequest;
+import com.digitalpetri.modbus.requests.WriteSingleCoilRequest;
+import com.digitalpetri.modbus.requests.WriteSingleRegisterRequest;
+import com.digitalpetri.modbus.responses.MaskWriteRegisterResponse;
+import com.digitalpetri.modbus.responses.ReadCoilsResponse;
+import com.digitalpetri.modbus.responses.ReadDiscreteInputsResponse;
 import com.digitalpetri.modbus.responses.ReadHoldingRegistersResponse;
+import com.digitalpetri.modbus.responses.ReadInputRegistersResponse;
+import com.digitalpetri.modbus.responses.WriteMultipleCoilsResponse;
+import com.digitalpetri.modbus.responses.WriteMultipleRegistersResponse;
+import com.digitalpetri.modbus.responses.WriteSingleCoilResponse;
+import com.digitalpetri.modbus.responses.WriteSingleRegisterResponse;
 import com.digitalpetri.modbus.slave.ModbusTcpSlave;
 import com.digitalpetri.modbus.slave.ModbusTcpSlaveConfig;
 import com.digitalpetri.modbus.slave.ServiceRequestHandler;
@@ -18,6 +34,50 @@ public class Slave {
 
 	public void start() throws ExecutionException, InterruptedException {
 		slave.setRequestHandler(new ServiceRequestHandler() {
+
+			@Override
+			public void onReadInputRegisters(ServiceRequest<ReadInputRegistersRequest, ReadInputRegistersResponse> service) {
+				ServiceRequestHandler.super.onReadInputRegisters(service);
+			}
+
+			@Override
+			public void onReadCoils(ServiceRequest<ReadCoilsRequest, ReadCoilsResponse> service) {
+				ServiceRequestHandler.super.onReadCoils(service);
+			}
+
+			@Override
+			public void onReadDiscreteInputs(ServiceRequest<ReadDiscreteInputsRequest, ReadDiscreteInputsResponse> service) {
+				ServiceRequestHandler.super.onReadDiscreteInputs(service);
+			}
+
+			@Override
+			public void onWriteSingleRegister(ServiceRequest<WriteSingleRegisterRequest, WriteSingleRegisterResponse> service) {
+				ServiceRequestHandler.super.onWriteSingleRegister(service);
+			}
+
+			@Override
+			public void onWriteMultipleCoils(ServiceRequest<WriteMultipleCoilsRequest, WriteMultipleCoilsResponse> service) {
+				ServiceRequestHandler.super.onWriteMultipleCoils(service);
+			}
+
+			@Override
+			public void onWriteMultipleRegisters(ServiceRequest<WriteMultipleRegistersRequest, WriteMultipleRegistersResponse> service) {
+				ServiceRequestHandler.super.onWriteMultipleRegisters(service);
+			}
+
+			@Override
+			public void onMaskWriteRegister(ServiceRequest<MaskWriteRegisterRequest, MaskWriteRegisterResponse> service) {
+				ServiceRequestHandler.super.onMaskWriteRegister(service);
+			}
+
+			@Override
+			public void onWriteSingleCoil(ServiceRequest<WriteSingleCoilRequest, WriteSingleCoilResponse> service) {
+				WriteSingleCoilRequest request = service.getRequest();
+
+				service.sendResponse(new WriteSingleCoilResponse(request.getAddress(), request.getValue()));
+
+				ReferenceCountUtil.release(request);
+			}
 
 			@Override
 			public void onReadHoldingRegisters(ServiceRequest<ReadHoldingRegistersRequest, ReadHoldingRegistersResponse> service) {
