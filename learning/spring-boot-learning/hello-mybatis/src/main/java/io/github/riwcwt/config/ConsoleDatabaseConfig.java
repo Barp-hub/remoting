ac7b0e3db020f39a5017a296f4804bae0e1dbb6e
zhu jun <zhujun@qbb6.com>
2017-02-14 13:17:59 +0800

    mybatis

--- learning/spring-boot-learning/hello-mybatis/src/main/java/io/github/riwcwt/config/ConsoleDatabaseConfig.java
+++ learning/spring-boot-learning/hello-mybatis/src/main/java/io/github/riwcwt/config/ConsoleDatabaseConfig.java
@@ -1,9 +1,6 @@
 package io.github.riwcwt.config;
 
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
+import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
@@ -17,40 +14,55 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.alibaba.druid.pool.DruidDataSource;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 
 @Configuration
 @EnableTransactionManagement
-@MapperScan(value = { "io.github.riwcwt.mapper" }, sqlSessionFactoryRef = "console-sqlSessionFactory")
+@MapperScan(value = {"io.github.riwcwt.mapper"}, sqlSessionFactoryRef = "console-sqlSessionFactory")
 public class ConsoleDatabaseConfig {
-	@Autowired
-	private Environment env;
+    @Autowired
+    private Environment env;
 
-	@Bean(name = "console-dataSource")
-	public DataSource dataSource() throws SQLException {
-		DruidDataSource dataSource = new DruidDataSource();
-		dataSource.setDriverClassName(env.getProperty("console.jdbc.driver-class-name"));
-		dataSource.setUrl(env.getProperty("console.jdbc.url"));
-		dataSource.setUsername(env.getProperty("console.jdbc.username"));
-		dataSource.setPassword(env.getProperty("console.jdbc.password"));
+    @Bean(name = "console-dataSource", initMethod = "init", destroyMethod = "close")
+    public DruidDataSource dataSource() throws SQLException {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(env.getProperty("console.jdbc.driver-class-name"));
+        dataSource.setUrl(env.getProperty("console.jdbc.url"));
+        dataSource.setUsername(env.getProperty("console.jdbc.username"));
+        dataSource.setPassword(env.getProperty("console.jdbc.password"));
 
-		dataSource.setFilters("log4j");
+        dataSource.setFilters("log4j");
+        dataSource.setMaxActive(20);
+        dataSource.setInitialSize(1);
+        dataSource.setMaxWait(60000);
+        dataSource.setMinIdle(1);
+        dataSource.setTimeBetweenEvictionRunsMillis(60000);
+        dataSource.setMinEvictableIdleTimeMillis(300000);
 
-		return dataSource;
-	}
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnBorrow(false);
+        dataSource.setTestOnReturn(false);
 
-	@Bean(name = "console-transactionManager")
-	public DataSourceTransactionManager transactionManager(@Qualifier("console-dataSource") DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource);
-	}
+        dataSource.setPoolPreparedStatements(true);
+        dataSource.setMaxOpenPreparedStatements(20);
 
-	@Bean(name = "console-sqlSessionFactory")
-	public SqlSessionFactory sqlSessionFactory(@Qualifier("console-dataSource") DataSource dataSource) throws Exception {
-		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-		sqlSessionFactoryBean.setDataSource(dataSource);
-		sqlSessionFactoryBean.setConfigLocation(new DefaultResourceLoader().getResource("classpath:mybatis/config.xml"));
-		sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/console/*.xml"));
-		return sqlSessionFactoryBean.getObject();
-	}
+        return dataSource;
+    }
+
+    @Bean(name = "console-transactionManager")
+    public DataSourceTransactionManager transactionManager(@Qualifier("console-dataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean(name = "console-sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("console-dataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setConfigLocation(new DefaultResourceLoader().getResource("classpath:mybatis/config.xml"));
+        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/console/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
 
 }
