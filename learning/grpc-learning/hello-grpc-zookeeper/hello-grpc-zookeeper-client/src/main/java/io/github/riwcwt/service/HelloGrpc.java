218fbe3a45ed217b757479f7b893e70e10ce024f
zhu jun <zhujun@qbb6.com>
2017-03-02 16:31:07 +0800

    zookeeper

---
+++ learning/grpc-learning/hello-grpc-zookeeper/hello-grpc-zookeeper-client/src/main/java/io/github/riwcwt/service/HelloGrpc.java
@@ -0,0 +1,15 @@
+package io.github.riwcwt.service;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by michael on 2017-03-02.
+ */
+@Component
+public class HelloGrpc implements InitializingBean {
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
