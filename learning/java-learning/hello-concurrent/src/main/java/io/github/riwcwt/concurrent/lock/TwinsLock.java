d598cbd6abd038066073f3f5efa0587ab611ac16
zhu jun <zhujun@qbb6.com>
2017-03-10 17:19:47 +0800

    java concurrent

---
+++ learning/java-learning/hello-concurrent/src/main/java/io/github/riwcwt/concurrent/lock/TwinsLock.java
@@ -0,0 +1,108 @@
+package io.github.riwcwt.concurrent.lock;
+
+import io.github.riwcwt.concurrent.SleepUtils;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Created by michael on 2017-03-10.
+ */
+public class TwinsLock implements Lock {
+
+
+    public static void main(String[] args) throws Exception {
+
+        final Lock lock = new TwinsLock();
+        class Worker extends Thread {
+            public void run() {
+                while (true) {
+                    lock.lock();
+                    try {
+                        SleepUtils.second(1);
+                        System.out.println(Thread.currentThread().getName());
+                        SleepUtils.second(1);
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }            // 启动10个线程
+        for (int i = 0; i < 10; i++) {
+            Worker w = new Worker();
+            w.setDaemon(true);
+            w.start();
+        }
+        // 每隔1秒换行
+        for (int i = 0; i < 10; i++) {
+            SleepUtils.second(1);
+            System.out.println();
+        }
+
+
+    }
+
+
+    private final Sync sync = new Sync(2);
+
+    private static final class Sync extends AbstractQueuedSynchronizer {
+        Sync(int count) {
+            if (count <= 0) {
+                throw new IllegalArgumentException("count must large                             than zero.");
+            }
+            setState(count);
+        }
+
+        public int tryAcquireShared(int reduceCount) {
+            for (; ; ) {
+                int current = getState();
+                int newCount = current - reduceCount;
+                if (newCount < 0 || compareAndSetState(current, newCount)) {
+                    return newCount;
+                }
+            }
+        }
+
+        public boolean tryReleaseShared(int returnCount) {
+            for (; ; ) {
+                int current = getState();
+                int newCount = current + returnCount;
+                if (compareAndSetState(current, newCount)) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    public void lock() {
+        sync.acquireShared(1);
+    }
+    // 其他接口方法略
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    public void unlock() {
+        sync.releaseShared(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+
+}
