dc93ad9864249d09dd7e9ed8c12f42b023685b95
zhu jun <zhujun@qbb6.com>
2017-04-17 09:52:12 +0800

    格式化代码

---
+++ learning/java-learning/hello-lambda/src/test/java/io/github/riwcwt/AsyncTest.java
@@ -0,0 +1,34 @@
+package io.github.riwcwt;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Created by michael on 2017-03-31.
+ */
+public class AsyncTest {
+
+    @Test
+    public void async() {
+        List<Integer> list = Arrays.asList(10, 20, 30, 40);
+        list.stream().map(data -> CompletableFuture.supplyAsync(() -> getNumber(data))).
+                map(compFuture -> compFuture.thenApply(n -> n * n)).map(t -> t.join())
+                .forEach(System.out::println);
+    }
+
+    private static int getNumber(int a) {
+        return a * a;
+    }
+
+    @Test
+    public void completable() {
+        CompletableFuture.supplyAsync(() -> 2)
+                .thenApply(i -> i * i)
+                .thenAccept(i -> System.out.println(i))
+                .whenComplete((result, error) -> System.out.println(result + " Error:" + error));
+    }
+
+}
