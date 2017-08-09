a4e43e30207d9b67153bd2380d56cd51eeef7326
zhu jun <zhujun@qbb6.com>
2017-08-09 15:28:39 +0800

    初始导入

---
+++ learning/spring-boot-learning/hello-solr/src/main/java/io/github/riwcwt/hellosolr/HelloSolrApplication.java
@@ -0,0 +1,12 @@
+package io.github.riwcwt.hellosolr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSolrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSolrApplication.class, args);
+	}
+}
