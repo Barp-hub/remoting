37048537c66cfee6cbea74065c164c446c38e903
zhu jun <zhujun@qbb6.com>
2017-11-10 18:49:23 +0800

    初始导入

--- learning/java-learning/hello-crawler/src/test/java/io/github/riwcwt/ApplicationTest.java
+++ learning/java-learning/hello-crawler/src/test/java/io/github/riwcwt/ApplicationTest.java
@@ -1,12 +1,17 @@
 package io.github.riwcwt;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Unit test for simple Application.
  */
-public class ApplicationTest
-{
+public class ApplicationTest {
+    @org.junit.Test
+    public void file() throws IOException {
+        FileUtils.copyURLToFile(new URL("https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-17.09.0.ce-1.el7.centos.x86_64.rpm"), new File("docker.rpm"));
+    }
 }
