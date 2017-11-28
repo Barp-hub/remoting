d4b9a9d6c504b41b3552a20cd1667589982855de
zhu jun <zhujun@qbb6.com>
2017-11-28 14:03:40 +0800

    初始导入

---
+++ linux/python/hello-python/game/functions.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+
+
+def check_event(ship):
+    # 监视键盘和鼠标事件
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                # 向右移动飞船
+                ship.rect.centerx += 1
+
+
+def update_screen(setting, screen, ship):
+    """更新屏幕上的图像，并切换到新屏幕"""
+    # 每次循环时都重绘屏幕
+    screen.fill(setting.bg_color)
+    ship.blitme()
+
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
--- linux/python/hello-python/hello_game.py
+++ linux/python/hello-python/hello_game.py
@@ -1,9 +1,27 @@
 import sys
 import pygame
+import game.setting
+import game.ship
+import game.functions
+
+setting = game.setting.Settings()
 
 
 def run_game():
     pygame.init()
+    screen = pygame.display.set_mode((setting.screen_width, setting.screen_height))
+    pygame.display.set_caption(setting.title)
+
+    ship = game.ship.Ship(screen)
+
+    # # 设置背景色
+    # bg_color = (230, 230, 230)
+
+    # 开始游戏的主循环
+    while True:
+        game.functions.check_event(ship)
+
+        game.functions.update_screen(setting, screen, ship)
 
 
 run_game()
