d8ed56089f7078f6f8b6bb82898cba39d769a42b
zhu jun <zhujun@qbb6.com>
2018-03-09 16:21:56 +0800

    初始导入

--- learning/java-learning/hello-simple-jvm/src/main/java/io/github/riwcwt/Application.java
+++ learning/java-learning/hello-simple-jvm/src/main/java/io/github/riwcwt/Application.java
@@ -1,12 +1,18 @@
 package io.github.riwcwt;
 
 import io.github.riwcwt.util.JvmUtil;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Hello world!
  */
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("is jdk : " + JvmUtil.isJDK());
+        FileUtils.copyURLToFile(new URL("https://stlib.qbb6.com/cnt0/chapter/0/2110rnCoNB/audioCourse6751777535067056001.mp3"), new File("./001.mp3"));
     }
 }
